8678172f8e11e00511bbdfdd9d1659dce3b40a4a
Noreenaysha <150052487+Noreenaysha@users.noreply.github.com>
2024-03-06 15:59:23 +0530

    Add files via upload

---
+++ multiplication.java
@@ -0,0 +1,16 @@
+class Multi{
+
+    public static void main(String args[])
+         {
+          System.out.println("***Multiplication Table***");
+                 int[] arr;
+                 arr= new int[11];
+
+                 for(int i=1;i<11;i++){
+                            arr[i]=i*5;
+
+                            System.out.println(i+"*5="+arr[i]);
+                    }
+
+         }
+}
